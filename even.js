b526f43681793e0fd58dce3e7ee00bcf7e1e8985
vigneshkumarb2023 <vigneshkumar.b2023lece@sece.ac.in>
2025-01-22 11:16:00 +0530

    vicky

---
+++ even.js
@@ -0,0 +1,22 @@
+//
+let a=5;
+for(i=0;i<=15;i++){
+    console.log(i ,"* 5 =",i*5  );
+}
+//
+let sum =0;
+for(i=0;i<=50;i++){
+    sum=sum+i;
+}
+console.log("Sum of numbers from 1 - 50 is ",sum);
+
+//
+console.log("Pattern printing");
+for(i=1;i<=5;i++){
+    console.log("*".repeat(i));
+}
+
+//
+const pattern = (n) =>{
+    
+}
